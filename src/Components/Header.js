94c7a2b4cd601bbe6570f3ce8eb0ed77eba6c2e5
Thehsin93 <thehsinnew@gmail.com>
2024-05-29 18:20:30 +0400

    build for publishing latest2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -78,7 +78,7 @@ const scrollToSection = (sectionRef)=>{
       
       )}
        
-       <div className='flex flex-row md:hidden p-5  shadow-lg justify-between fixed mb-[5%] mt-[195%] bg-gray-50 z-30'>
+       <div className='flex flex-row md:hidden p-5  shadow-lg justify-between fixed mb-[5%] mt-[175%] bg-gray-50 z-30'>
     <div className='w-[70%] flex flex-row'>
           <span>Thehsin</span>
         
